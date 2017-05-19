41bf5d4446d819c5d127a6169ca2fb1d2bda5199
Dan Gohman <sunfish@mozilla.com>
2017-05-19 16:48:17 -0700

    Fix GVN skipping the instruction after a deleted instruction.

--- lib/cretonne/src/simple_gvn.rs
+++ lib/cretonne/src/simple_gvn.rs
@@ -47,7 +47,7 @@ pub fn do_simple_gvn(func: &mut Function, cfg: &mut ControlFlowGraph) {
                 Occupied(mut entry) => {
                     if domtree.dominates(*entry.get(), inst, &pos.layout) {
                         func.dfg.replace_with_aliases(inst, *entry.get());
-                        pos.remove_inst();
+                        pos.remove_inst_and_step_back();
                     } else {
                         // The prior instruction doesn't dominate inst, so it
                         // won't dominate any subsequent instructions we'll
