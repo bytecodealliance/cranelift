ff93cb5a1c12fc6090c450d5060e5b1449d3a20c
Denis Merigoux <denis.merigoux@gmail.com>
2017-06-05 09:24:17 -0700

    Bugfix: moved instructions inserted at right place If the LICM pass detects a natural pre-header, it should insert invariant instructions before the jump to the header and not necessarily at the end of the block.

--- lib/cretonne/src/licm.rs
+++ lib/cretonne/src/licm.rs
@@ -23,20 +23,29 @@ pub fn do_licm(func: &mut Function,
         // Then we remove the invariant instructions from the loop body
         if invariant_inst.len() > 0 {
             // If the loop has a natural pre-header we use it, otherwise we create it.
-            let pre_header = match has_pre_header(&func.layout,
-                                                  cfg,
-                                                  domtree,
-                                                  loop_analysis.loop_header(lp).clone()) {
+            let mut pos;
+            match has_pre_header(&func.layout,
+                                 cfg,
+                                 domtree,
+                                 loop_analysis.loop_header(lp).clone()) {
                 None => {
-                    create_pre_header(loop_analysis.loop_header(lp).clone(), func, cfg, domtree)
+                    let pre_header = create_pre_header(loop_analysis.loop_header(lp).clone(),
+                                                       func,
+                                                       cfg,
+                                                       domtree);
+                    pos = Cursor::new(&mut func.layout);
+                    pos.goto_bottom(pre_header);
+                    pos.prev_inst();
                 }
-                Some(pre_hd) => pre_hd,
+                // If there is a natural pre-header we insert new instructions just before the
+                // related jumping instruction (which is not necessarily at the end).
+                Some((_, last_inst)) => {
+                    pos = Cursor::new(&mut func.layout);
+                    pos.goto_inst(last_inst);
+                }
             };
-            let mut pos = Cursor::new(&mut func.layout);
-            pos.goto_bottom(pre_header);
             // The last instruction of the pre-header is the termination instruction (usually
             // a jump) so we need to insert just before this.
-            pos.prev_inst();
             for inst in invariant_inst.iter() {
                 pos.insert_inst(inst.clone());
             }
@@ -93,11 +102,12 @@ fn create_pre_header(header: Ebb,
 //
 // A loop header has a pre-header if there is only one predecessor that the header doesn't
 // dominate.
+// Returns the pre-header Ebb and the instruction jumping to  the header.
 fn has_pre_header(layout: &Layout,
                   cfg: &ControlFlowGraph,
                   domtree: &DominatorTree,
                   header: Ebb)
-                  -> Option<Ebb> {
+                  -> Option<(Ebb, Inst)> {
     let mut result = None;
     let mut found = false;
     for &(pred_ebb, last_inst) in cfg.get_predecessors(header) {
@@ -107,7 +117,7 @@ fn has_pre_header(layout: &Layout,
                 // We have already found one, there are more than one
                 return None;
             } else {
-                result = Some(pred_ebb);
+                result = Some((pred_ebb, last_inst));
                 found = true;
             }
         }
