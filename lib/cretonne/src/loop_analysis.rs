a05653bed5d402ae081b6bcd20009ab3480de3b6
Denis Merigoux <denis.merigoux@gmail.com>
2017-08-01 18:35:38 -0700

    Added last instruction information to loop analysis Loop analysis now performs ebb splitting in case of multi-loop ebbs

--- lib/cretonne/src/loop_analysis.rs
+++ lib/cretonne/src/loop_analysis.rs
@@ -5,7 +5,8 @@ use dominator_tree::DominatorTree;
 use entity::{PrimaryMap, Keys};
 use entity::EntityMap;
 use flowgraph::ControlFlowGraph;
-use ir::{Function, Ebb, Layout};
+use ir::{Function, Ebb, Layout, Cursor, InstBuilder};
+use ir::entities::Inst;
 use packed_option::PackedOption;
 
 /// A opaque reference to a code loop.
@@ -18,8 +19,8 @@ entity_impl!(Loop, "loop");
 /// Loops are referenced by the Loop object, and for each loop you can access its header EBB,
 /// its eventual parent in the loop tree and all the EBB belonging to the loop.
 pub struct LoopAnalysis {
-    loops: PrimaryMap<Loop, LoopData>,
-    ebb_loop_map: EntityMap<Ebb, PackedOption<Loop>>,
+    loops: EntityMap<Loop, LoopData>,
+    ebb_loop_map: EntityMap<Ebb, EbbLoopData>,
 }
 
 struct LoopData {
@@ -29,11 +30,30 @@ struct LoopData {
 
 impl LoopData {
     /// Creates a `LoopData` object with the loop header and its eventual parent in the loop tree.
-    pub fn new(header: Ebb, parent: Option<Loop>) -> LoopData {
+    fn new(header: Ebb, parent: Option<Loop>) -> LoopData {
         LoopData {
             header: header,
             parent: parent.into(),
         }
+    }
+}
+
+/// If an `Ebb` is part of a loop, then we record two things: the id of the loop it's part of
+/// and the last instruction in the `Ebb` pertaining to the loop. If the `Ebb` is part of multiple
+/// loops, then we make sure by splitting the `Ebb` that it is part of at most two loops, one being
+/// the direct child of the other, and the parent loop should contain all the `Ebb` up to its
+/// terminator instruction.
+#[derive(Clone,Debug)]
+enum EbbLoopData {
+    NoLoop(),
+    Loop { loop_id: Loop, last_inst: Inst },
+}
+
+impl PrimaryEntityData for EbbLoopData {}
+
+impl Default for EbbLoopData {
+    fn default() -> EbbLoopData {
+        EbbLoopData::NoLoop()
     }
 }
 
@@ -66,14 +86,32 @@ impl LoopAnalysis {
         self.loops[lp].parent.expand()
     }
 
-    /// Determine if an Ebb belongs to a loop by running a finger along the loop tree.
+    /// Determine if an `Ebb` belongs to a loop by running a finger along the loop tree.
     ///
     /// Returns `true` if `ebb` is in loop `lp`.
     pub fn is_in_loop(&self, ebb: Ebb, lp: Loop) -> bool {
-        let ebb_loop = self.ebb_loop_map[ebb];
-        match ebb_loop.expand() {
-            None => false,
-            Some(ebb_loop) => self.is_child_loop(ebb_loop, lp),
+        match self.ebb_loop_map[ebb] {
+            EbbLoopData::NoLoop() => false,
+            EbbLoopData::Loop { loop_id, .. } => self.is_child_loop(loop_id, lp),
+        }
+    }
+
+    /// Determine which region of an `Ebb` belongs to a particular loop.
+    ///
+    /// If `ebb` belongs to `lp`, it returns `None` if the whole `ebb` belongs to `lp` or
+    /// `Some(inst)` where `inst` is the last instruction of `ebb` to belong to `lp`.
+    pub fn last_loop_instruction(&self, ebb: Ebb, lp: Loop) -> Result<Option<Inst>, ()> {
+        match self.ebb_loop_map[ebb] {
+            EbbLoopData::NoLoop() => Err(()),
+            EbbLoopData::Loop { loop_id, last_inst } => {
+                if lp == loop_id {
+                    Ok(Some(last_inst))
+                } else if self.is_child_loop(loop_id, lp) {
+                    Ok(None)
+                } else {
+                    Err(())
+                }
+            }
         }
     }
 
@@ -95,12 +133,15 @@ impl LoopAnalysis {
 
 impl LoopAnalysis {
     /// Detects the loops in a function. Needs the control flow graph and the dominator tree.
-    pub fn compute(&mut self, func: &Function, cfg: &ControlFlowGraph, domtree: &DominatorTree) {
+    pub fn compute(&mut self,
+                   func: &mut Function,
+                   cfg: &mut ControlFlowGraph,
+                   domtree: &mut DominatorTree) {
         self.loops.clear();
         self.ebb_loop_map.clear();
         self.ebb_loop_map.resize(func.dfg.num_ebbs());
         self.find_loop_headers(cfg, domtree, &func.layout);
-        self.discover_loop_blocks(cfg, domtree, &func.layout)
+        self.discover_loop_blocks(cfg, domtree, func)
     }
 
     // Traverses the CFG in reverse postorder and create a loop object for every EBB having a
@@ -115,8 +156,7 @@ impl LoopAnalysis {
                 // If the ebb dominates one of its predecessors it is a back edge
                 if domtree.dominates(ebb, pred_inst, layout) {
                     // This ebb is a loop header, so we create its associated loop
-                    let lp = self.loops.push(LoopData::new(ebb, None));
-                    self.ebb_loop_map[ebb] = lp.into();
+                    self.loops.push(LoopData::new(ebb, None));
                     break;
                     // We break because we only need one back edge to identify a loop header.
                 }
@@ -128,77 +168,156 @@ impl LoopAnalysis {
     // discovers all the ebb belonging to the loop and its inner loops. After a call to this
     // function, the loop tree is fully constructed.
     fn discover_loop_blocks(&mut self,
-                            cfg: &ControlFlowGraph,
-                            domtree: &DominatorTree,
-                            layout: &Layout) {
-        let mut stack: Vec<Ebb> = Vec::new();
+                            cfg: &mut ControlFlowGraph,
+                            domtree: &mut DominatorTree,
+                            func: &mut Function) {
+        let mut stack: Vec<(Ebb, Inst)> = Vec::new();
         // We handle each loop header in reverse order, corresponding to a pesudo postorder
         // traversal of the graph.
         for lp in self.loops().rev() {
             for &(pred, pred_inst) in cfg.get_predecessors(self.loops[lp].header) {
-                // We follow the back edges
-                if domtree.dominates(self.loops[lp].header, pred_inst, layout) {
-                    stack.push(pred);
+                // We follow the back edges only
+                if domtree.dominates(self.loops[lp].header, pred_inst, &func.layout) {
+                    stack.push((pred, pred_inst));
                 }
             }
-            while let Some(node) = stack.pop() {
+            // Then we proceed to discover loop blocks by doing a "reverse" DFS
+            while let Some((ebb, loop_edge_inst)) = stack.pop() {
                 let continue_dfs: Option<Ebb>;
-                match self.ebb_loop_map[node].expand() {
-                    None => {
-                        // The node hasn't been visited yet, we tag it as part of the loop
-                        self.ebb_loop_map[node] = PackedOption::from(lp);
-                        continue_dfs = Some(node);
+                match self.ebb_loop_map[ebb] {
+                    EbbLoopData::NoLoop() => {
+                        // The ebb hasn't been visited yet, we tag it as part of the loop
+                        self.ebb_loop_map[ebb] = EbbLoopData::Loop {
+                            loop_id: lp,
+                            last_inst: loop_edge_inst,
+                        };
+                        // We stop the DFS if the block is the header of the loop
+                        if ebb != self.loops[lp].header {
+                            continue_dfs = Some(ebb);
+                        } else {
+                            continue_dfs = None;
+                        }
                     }
-                    Some(node_loop) => {
-                        // We copy the node_loop into a mutable reference passed along the while
-                        let mut node_loop = node_loop;
-                        // The node is part of a loop, which can be lp or an inner loop
-                        let mut node_loop_parent_option = self.loops[node_loop].parent;
-                        while let Some(node_loop_parent) = node_loop_parent_option.expand() {
-                            if node_loop_parent == lp {
+                    EbbLoopData::Loop {
+                        loop_id,
+                        last_inst: _,
+                    } => {
+                        // So here the ebb we're visiting is already tagged as being part of a loop
+                        // But since we're considering the loop headers in postorder, that loop
+                        // can only be an inner loop of the loop we're considering.
+                        // If this is the first time we reach this inner loop, we declare our loop
+                        // the parent loop and continue the DFS at the header block of the inner
+                        // loop.
+                        // If we have already visited this inner loop, we can stop the DFS here.
+                        let mut current_loop = loop_id;
+                        let mut current_loop_parent_option = self.loops[current_loop].parent;
+                        while let Some(current_loop_parent) = current_loop_parent_option.expand() {
+                            if current_loop_parent == lp {
                                 // We have encounterd lp so we stop (already visited)
                                 break;
                             } else {
-                                //
-                                node_loop = node_loop_parent;
+                                current_loop = current_loop_parent;
                                 // We lookup the parent loop
-                                node_loop_parent_option = self.loops[node_loop].parent;
+                                current_loop_parent_option = self.loops[current_loop].parent;
                             }
                         }
-                        // Now node_loop_parent is either:
-                        // - None and node_loop is an new inner loop of lp
-                        // - Some(...) and the initial node_loop was a known inner loop of lp
-                        match node_loop_parent_option.expand() {
+                        // Now current_loop_parent is either:
+                        // - None and current_loop is an new inner loop of lp
+                        // - Some(...) and the initial node loop_id was a known inner loop of lp,
+                        //   in this case we have already visited it and we stop the DFS
+                        match current_loop_parent_option.expand() {
                             Some(_) => continue_dfs = None,
                             None => {
-                                if node_loop != lp {
-                                    self.loops[node_loop].parent = lp.into();
-                                    continue_dfs = Some(self.loops[node_loop].header)
-                                } else {
-                                    // If lp is a one-block loop then we make sure we stop
+                                let inner_loop = current_loop;
+                                let inner_loop_header = self.loops[inner_loop].header;
+                                if inner_loop == lp {
+                                    // If the inner loop is lp it means that we have reached a
+                                    // a block that has been previously tagged as part of lp so
+                                    // it has been visited by the DFS so we stop
                                     continue_dfs = None
+                                } else {
+                                    // Else we proceed to mark the inner loop as child of lp
+                                    // And continue the DFS with the inner loop's header
+                                    self.loops[inner_loop].parent = lp.into();
+                                    continue_dfs = Some(inner_loop_header);
+                                    // Here we perform a modification that's not related to
+                                    // the loop discovery algorithm but rather to the way we
+                                    // store the loop information. Indeed, for each Ebb we record
+                                    // the loop its part of and the last inst in this loop
+                                    // But here the Ebb is part of at least two loops, the child
+                                    // loop and the parent loop . So two cases arise:
+                                    // - either the entire ebb is part of the parent loop and this
+                                    // is fine;
+                                    // - either only part of the ebb is part of the parent loop and
+                                    // in that case we can't store the information of where does
+                                    // the parent loop stops.
+                                    // In the second case, we proceed to split the Ebb just before
+                                    // the parent's loop branch instruction (which is not a
+                                    // terminator instruction) so that now the original ebb is
+                                    // entirely in the parent loop, and the one we created by
+                                    // splitting is part of only one loop, the parent loop.
+                                    if func.layout
+                                           .last_inst(ebb)
+                                           .map_or(false, |ebb_last_inst| {
+                                        ebb_last_inst != loop_edge_inst
+                                    }) {
+                                        // This handles the second case
+                                        self.split_ebb_containing_two_loops(ebb,
+                                                                            loop_edge_inst,
+                                                                            lp,
+                                                                            func,
+                                                                            domtree,
+                                                                            cfg);
+                                    }
                                 }
                             }
                         }
                     }
                 }
-                // Now we have handled the popped node and need to continue the DFS by adding the
-                // predecessors of that node
+                // Now we have handled the popped Ebb and need to continue the DFS by adding the
+                // predecessors of that Ebb
                 if let Some(continue_dfs) = continue_dfs {
-                    for &(pred, _) in cfg.get_predecessors(continue_dfs) {
-                        stack.push(pred)
+                    for &(pre, pre_inst) in cfg.get_predecessors(continue_dfs) {
+                        stack.push((pre, pre_inst))
                     }
                 }
             }
-
         }
+    }
+
+    // We are in the case where `ebb` belongs partially to two different loops, the child and
+    // the parent. `lp` here is the parent loop and we create a new Ebb so that `ebb` belongs
+    // in its entirety to the parent loop.
+    // We also update the cfd and domtree to reflect that.
+    fn split_ebb_containing_two_loops(&mut self,
+                                      ebb: Ebb,
+                                      split_inst: Inst,
+                                      lp: Loop,
+                                      func: &mut Function,
+                                      domtree: &mut DominatorTree,
+                                      cfg: &mut ControlFlowGraph) {
+        let new_ebb = func.dfg.make_ebb();
+        func.layout.split_ebb(new_ebb, split_inst);
+        {
+            let cur = &mut Cursor::new(&mut func.layout);
+            cur.goto_bottom(ebb);
+            func.dfg.ins(cur).jump(new_ebb, &[]);
+        }
+        *self.ebb_loop_map.ensure(new_ebb) = EbbLoopData::Loop {
+            loop_id: lp,
+            last_inst: split_inst,
+        };
+        cfg.recompute_ebb(func, ebb);
+        cfg.recompute_ebb(func, new_ebb);
+        domtree.compute(func, cfg);
     }
 }
 
 #[cfg(test)]
 mod test {
 
-    use ir::{Function, InstBuilder, Cursor, CursorBase, types};
+    use ir::{Function, InstBuilder, Cursor, types};
+    use ir::entities::Ebb;
     use loop_analysis::{Loop, LoopAnalysis};
     use flowgraph::ControlFlowGraph;
     use dominator_tree::DominatorTree;
@@ -236,7 +355,7 @@ mod test {
         let mut domtree = DominatorTree::new();
         cfg.compute(&func);
         domtree.compute(&func, &cfg);
-        loop_analysis.compute(&func, &cfg, &domtree);
+        loop_analysis.compute(&mut func, &mut cfg, &mut domtree);
 
         let loops = loop_analysis.loops().collect::<Vec<Loop>>();
         assert_eq!(loops.len(), 2);
@@ -297,7 +416,7 @@ mod test {
         let mut domtree = DominatorTree::new();
         cfg.compute(&func);
         domtree.compute(&func, &cfg);
-        loop_analysis.compute(&func, &cfg, &domtree);
+        loop_analysis.compute(&mut func, &mut cfg, &mut domtree);
 
         let loops = loop_analysis.loops().collect::<Vec<Loop>>();
         assert_eq!(loops.len(), 3);
@@ -313,5 +432,59 @@ mod test {
         assert_eq!(loop_analysis.is_in_loop(ebb3, loops[2]), true);
         assert_eq!(loop_analysis.is_in_loop(ebb4, loops[2]), true);
         assert_eq!(loop_analysis.is_in_loop(ebb5, loops[0]), true);
+    }
+
+    #[test]
+    fn ebb_splitting() {
+        let mut func = Function::new();
+        let ebb0 = func.dfg.make_ebb();
+        let ebb1 = func.dfg.make_ebb();
+        let ebb2 = func.dfg.make_ebb();
+        let cond = func.dfg.append_ebb_arg(ebb0, types::I32);
+
+        let (inst0, inst1) = {
+            let dfg = &mut func.dfg;
+            let cur = &mut Cursor::new(&mut func.layout);
+
+            cur.insert_ebb(ebb0);
+            dfg.ins(cur).jump(ebb1, &[]);
+
+            cur.insert_ebb(ebb1);
+            let inst0 = dfg.ins(cur).brnz(cond, ebb1, &[]);
+            let inst1 = dfg.ins(cur).brnz(cond, ebb0, &[]);
+            dfg.ins(cur).jump(ebb2, &[]);
+
+            cur.insert_ebb(ebb2);
+            dfg.ins(cur).return_(&[]);
+            (inst0, inst1)
+        };
+
+        let mut loop_analysis = LoopAnalysis::new();
+        let mut cfg = ControlFlowGraph::new();
+        let mut domtree = DominatorTree::new();
+        cfg.compute(&func);
+        domtree.compute(&func, &cfg);
+        loop_analysis.compute(&mut func, &mut cfg, &mut domtree);
+
+        let loops = loop_analysis.loops().collect::<Vec<Loop>>();
+        assert_eq!(loops.len(), 2);
+        assert_eq!(loop_analysis.loop_header(loops[0]), ebb0);
+        assert_eq!(loop_analysis.loop_header(loops[1]), ebb1);
+        assert_eq!(loop_analysis.loop_parent(loops[1]), Some(loops[0]));
+        assert_eq!(loop_analysis.is_in_loop(ebb0, loops[0]), true);
+        assert_eq!(loop_analysis.is_in_loop(ebb1, loops[1]), true);
+        assert_eq!(loop_analysis.is_in_loop(ebb2, loops[0]), false);
+        assert_eq!(loop_analysis.is_in_loop(Ebb::with_number(3).unwrap(), loops[0]),
+                   true);
+        assert_eq!(loop_analysis
+                       .last_loop_instruction(ebb1, loops[1])
+                       .unwrap()
+                       .unwrap(),
+                   inst0);
+        assert_eq!(loop_analysis
+                       .last_loop_instruction(Ebb::with_number(3).unwrap(), loops[0])
+                       .unwrap()
+                       .unwrap(),
+                   inst1)
     }
 }
