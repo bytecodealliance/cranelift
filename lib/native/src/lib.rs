cf9dd593e1bb0750ed5dacdfd73e61e1c47d931b
Dan Gohman <sunfish@mozilla.com>
2017-09-20 15:45:18 -0700

    Use the proper builder for enabling subtarget flags.

--- lib/native/src/lib.rs
+++ lib/native/src/lib.rs
@@ -22,67 +22,67 @@ pub fn builders() -> Result<(settings::Builder, isa::Builder), ()> {
 
     // TODO: Add RISC-V support once Rust supports it.
 
-    let name = if cfg!(target_arch = "x86") {
-        parse_x86_cpuid(&mut flag_builder);
-        "intel"
-    } else if cfg!(target_arch = "x86_64") {
+    if cfg!(target_pointer_width = "64") {
         flag_builder.enable("is_64bit").unwrap();
-        parse_x86_cpuid(&mut flag_builder);
+    }
+
+    let name = if cfg!(any(target_arch = "x86", target_arch = "x86_64")) {
         "intel"
     } else if cfg!(target_arch = "arm") {
         "arm32"
     } else if cfg!(target_arch = "aarch64") {
-        flag_builder.enable("is_64bit").unwrap();
         "arm64"
     } else {
         return Err(());
     };
 
-    isa::lookup(name).map(|x| (flag_builder, x)).map_err(
-        |err| {
-            match err {
-                isa::LookupError::Unknown => panic!(),
-                isa::LookupError::Unsupported => (()),
-            }
-        },
-    )
+    let mut isa_builder = isa::lookup(name).map_err(|err| match err {
+        isa::LookupError::Unknown => panic!(),
+        isa::LookupError::Unsupported => (()),
+    })?;
+
+    if cfg!(any(target_arch = "x86", target_arch = "x86_64")) {
+        parse_x86_cpuid(&mut isa_builder);
+    }
+
+    Ok((flag_builder, isa_builder))
 }
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-fn parse_x86_cpuid(flag_builder: &mut settings::Builder) {
+fn parse_x86_cpuid(isa_builder: &mut isa::Builder) {
     let cpuid = CpuId::new();
 
     if let Some(info) = cpuid.get_feature_info() {
         if info.has_sse2() {
-            flag_builder.enable("has_sse2").unwrap();
+            isa_builder.enable("has_sse2").unwrap();
         }
         if info.has_sse3() {
-            flag_builder.enable("has_sse3").unwrap();
+            isa_builder.enable("has_sse3").unwrap();
         }
         if info.has_sse41() {
-            flag_builder.enable("has_sse41").unwrap();
+            isa_builder.enable("has_sse41").unwrap();
         }
         if info.has_sse42() {
-            flag_builder.enable("has_sse42").unwrap();
+            isa_builder.enable("has_sse42").unwrap();
         }
         if info.has_popcnt() {
-            flag_builder.enable("has_popcnt").unwrap();
+            isa_builder.enable("has_popcnt").unwrap();
         }
         if info.has_avx() {
-            flag_builder.enable("has_avx").unwrap();
+            isa_builder.enable("has_avx").unwrap();
         }
     }
     if let Some(info) = cpuid.get_extended_feature_info() {
         if info.has_bmi1() {
-            flag_builder.enable("has_bmi1").unwrap();
+            isa_builder.enable("has_bmi1").unwrap();
         }
         if info.has_bmi2() {
-            flag_builder.enable("has_bmi2").unwrap();
+            isa_builder.enable("has_bmi2").unwrap();
         }
     }
     if let Some(info) = cpuid.get_extended_function_info() {
         if info.has_lzcnt() {
-            flag_builder.enable("has_lzcnt").unwrap();
+            isa_builder.enable("has_lzcnt").unwrap();
         }
     }
 }
