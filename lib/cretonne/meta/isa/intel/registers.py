48c883b6cdc79bb4120783cda41a536f7e18d6ae
Tyler McMullen <tyler@fastly.com>
2018-01-15 22:26:54 -0800

    Add special RegClass to remove r13 from 'ld' recipe.

--- lib/cretonne/meta/isa/intel/registers.py
+++ lib/cretonne/meta/isa/intel/registers.py
@@ -46,7 +46,9 @@ FlagRegs = RegBank(
         names=['eflags'])
 
 GPR = RegClass(IntRegs)
+GPR_NORIP = GPR.without(GPR.rbp, GPR.r13)
 GPR8 = GPR[0:8]
+GPR8_NORIP = GPR8.without(GPR.rbp)
 ABCD = GPR[0:4]
 FPR = RegClass(FloatRegs)
 FPR8 = FPR[0:8]
