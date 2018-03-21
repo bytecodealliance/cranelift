0f4b995ab30ac8513bf064ae0f2da5ab9eb39f31
Pat Hickey <pat@moreproductive.org>
2018-03-21 14:33:25 -0700

    cton-wasm: fix clippy complaints or ignore benign ones

--- lib/wasm/src/lib.rs
+++ lib/wasm/src/lib.rs
@@ -14,6 +14,8 @@
         unused_extern_crates)]
 #![cfg_attr(feature="clippy",
             plugin(clippy(conf_file="../../clippy.toml")))]
+#![cfg_attr(feature="cargo-clippy",
+            allow(new_without_default, redundant_field_names))]
 
 extern crate wasmparser;
 extern crate cton_frontend;
