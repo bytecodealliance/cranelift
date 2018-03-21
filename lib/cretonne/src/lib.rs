36a5d79698ee9acf6d0adc1d4d6462897d897b8f
Pat Hickey <pat@moreproductive.org>
2018-03-21 16:37:05 -0700

    cretonne: starting point turns off all clippy warnings

--- lib/cretonne/src/lib.rs
+++ lib/cretonne/src/lib.rs
@@ -4,13 +4,49 @@
         trivial_numeric_casts,
         unused_extern_crates)]
 
+#![cfg_attr(feature="clippy",
+            plugin(clippy(conf_file="../../clippy.toml")))]
+// Most of these are required for the generated code to pass clippy:
+#![cfg_attr(feature="cargo-clippy",
+            allow(new_without_default,
+                  new_without_default_derive,
+                  redundant_field_names,
+                  many_single_char_names,
+                  match_same_arms,
+                  should_implement_trait,
+                  identity_op,
+                  needless_borrow,
+                  cyclomatic_complexity,
+                  too_many_arguments,
+                  cast_lossless,
+                  assign_op_pattern,
+                  unreadable_literal,
+                  empty_line_after_outer_attr,
+                  // From here on i think theyre solvable:
+                  wrong_self_convention,
+                  needless_pass_by_value,
+                  block_in_if_condition_stmt,
+                  or_fun_call,
+                  collapsible_if,
+                  needless_lifetimes,
+                  enum_variant_names,
+                  match_wild_err_arm,
+                  useless_let_if_seq,
+                  len_without_is_empty,
+                  single_match,
+                  unused_lifetimes,
+                  match_ref_pats,
+                  iter_skip_next,
+                  while_let_loop,
+                  doc_markdown))]
+
 pub use context::Context;
 pub use legalizer::legalize_function;
 pub use verifier::verify_function;
 pub use write::write_function;
 
 /// Version number of the cretonne crate.
-pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[macro_use]
 pub mod dbg;
