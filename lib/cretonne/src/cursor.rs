7694c66689b3840733a4f2c461b2283ebae550b6
Pat Hickey <pat@moreproductive.org>
2018-03-22 12:36:32 -0700

    rustfmt

--- lib/cretonne/src/cursor.rs
+++ lib/cretonne/src/cursor.rs
@@ -746,7 +746,7 @@ impl<'c, 'f> ir::InstInserterBase<'c> for &'c mut EncCursor<'f> {
         }
         // Assign an encoding.
         // XXX Is there a way to describe this error to the user?
-        #[cfg_attr(feature="cargo-clippy", allow(match_wild_err_arm))]
+        #[cfg_attr(feature = "cargo-clippy", allow(match_wild_err_arm))]
         match self.isa.encode(
             &self.func.dfg,
             &self.func.dfg[inst],
