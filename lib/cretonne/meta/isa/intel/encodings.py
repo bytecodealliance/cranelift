cbc27c2c0f56a51b2d127b7534975d5045cd3400
Pat Hickey <pat@moreproductive.org>
2018-03-26 14:22:52 -0700

    shift immediate encodings: use enc_i32_i64
    and note why the regular shift encodings cant use it above

--- lib/cretonne/meta/isa/intel/encodings.py
+++ lib/cretonne/meta/isa/intel/encodings.py
@@ -164,6 +164,8 @@ for inst,           rrr in [
         (base.ishl, 4),
         (base.ushr, 5),
         (base.sshr, 7)]:
+    # Cannot use enc_i32_i64 for this pattern because instructions require
+    # .any suffix.
     X86_32.enc(inst.i32.any, *r.rc(0xd3, rrr=rrr))
     X86_64.enc(inst.i64.any, *r.rc.rex(0xd3, rrr=rrr, w=1))
     X86_64.enc(inst.i32.any, *r.rc.rex(0xd3, rrr=rrr))
@@ -173,10 +175,7 @@ for inst,           rrr in [
         (base.ishl_imm, 4),
         (base.ushr_imm, 5),
         (base.sshr_imm, 7)]:
-    X86_32.enc(inst.i32, *r.rib(0xc1, rrr=rrr))
-    X86_64.enc(inst.i64, *r.rib.rex(0xc1, rrr=rrr, w=1))
-    X86_64.enc(inst.i32, *r.rib.rex(0xc1, rrr=rrr))
-    X86_64.enc(inst.i32, *r.rib(0xc1, rrr=rrr))
+    enc_i32_i64(inst, r.rib, 0xc1, rrr=rrr)
 
 # Population count.
 X86_32.enc(base.popcnt.i32, *r.urm(0xf3, 0x0f, 0xb8), isap=cfg.use_popcnt)
