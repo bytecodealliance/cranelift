81d1510318ef2f9003554d44b5fb2ab85c17a95a
Jonathan Foote <jmfoote@loyola.edu>
2018-05-06 12:31:43 -0400

    check fuzzer integration via CI

--- fuzz/fuzz_translate_module.rs
+++ fuzz/fuzz_translate_module.rs
@@ -6,7 +6,7 @@ extern crate cretonne_wasm;
 use cretonne_wasm::{translate_module, DummyEnvironment};
 
 fuzz_target!(|data: &[u8]| {
-    let binaryen_module = binaryen::tools::translate_to_fuzz(data);
+    let binaryen_module = binaryen::tools::translate_to_fuzz_mvp(data);
 
     let wasm = binaryen_module.write();
 
