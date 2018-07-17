d8e94235333611955ad63cc91f2ba669d34e00d1
data-pup <kevinmartinucr@gmail.com>
2018-07-17 19:14:12 -0400

    Fixing a comment bug.

--- lib/codegen-meta/src/gen_types.rs
+++ lib/codegen-meta/src/gen_types.rs
@@ -4,7 +4,8 @@
 //! `lib/codegen/ir/types.rs`. The file provides constant definitions for the
 //! most commonly used types, including all of the scalar types.
 //!
-//! This ensures that Python and Rust use the same type numbering.
+//! This ensures that the metaprogram and the generated program see the same
+//! type numbering.
 
 use cdsl::types as cdsl_types;
 use error;
