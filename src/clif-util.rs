2f1023ad95f6481dea70233eafef9a6a2734e856
Caroline Cullen <cullen.caroline@gmail.com>
2018-08-13 16:38:20 -0700

    Changing from docopt to clap for the clif-util

--- src/clif-util.rs
+++ src/clif-util.rs
@@ -1,5 +1,5 @@
-#![deny(trivial_numeric_casts, unused_extern_crates, unstable_features)]
-#![warn(unused_import_braces)]
+#![deny(trivial_numeric_casts)]
+#![warn(unused_import_braces, unstable_features, unused_extern_crates)]
 #![cfg_attr(
     feature = "cargo-clippy",
     warn(
@@ -10,22 +10,18 @@
 
 #[macro_use]
 extern crate cfg_if;
-extern crate cranelift_codegen;
-extern crate cranelift_filetests;
-extern crate cranelift_reader;
-extern crate docopt;
-extern crate file_per_thread_logger;
-extern crate filecheck;
-#[macro_use]
-extern crate serde_derive;
 #[cfg(feature = "disas")]
 extern crate capstone;
-extern crate pretty_env_logger;
+extern crate clap;
+extern crate cranelift_codegen;
+extern crate cranelift_entity;
+extern crate cranelift_filetests;
+extern crate cranelift_reader;
+extern crate filecheck;
 extern crate term;
 
 cfg_if! {
     if #[cfg(feature = "wasm")] {
-        extern crate cranelift_entity;
         extern crate cranelift_wasm;
         extern crate wabt;
         mod wasm;
@@ -33,10 +29,10 @@ cfg_if! {
 }
 extern crate target_lexicon;
 
-use cranelift_codegen::dbg::LOG_FILENAME_PREFIX;
-use cranelift_codegen::{timing, VERSION};
-use docopt::Docopt;
+use clap::{App, Arg, SubCommand};
+use cranelift_codegen::VERSION;
 use std::io::{self, Write};
+use std::option::Option;
 use std::process;
 
 mod cat;
@@ -45,125 +41,179 @@ mod print_cfg;
 mod rsfilecheck;
 mod utils;
 
-const USAGE: &str = "
-Cranelift code generator utility
-
-Usage:
-    clif-util test [-vTd] <file>...
-    clif-util cat [-d] <file>...
-    clif-util filecheck [-vd] <file>
-    clif-util print-cfg [-d] <file>...
-    clif-util compile [-vpTd] [--set <set>]... [--target <triple>] <file>...
-    clif-util wasm [-ctvpTsd] [--set <set>]... [--target <triple>] <file>...
-    clif-util --help | --version
-
-Options:
-    -v, --verbose   be more verbose
-    -d, --debug     enable debug output on stderr/stdout
-    -T, --time-passes
-                    print pass timing report
-    -t, --just-decode
-                    just decode WebAssembly to Cranelift IR
-    -s, --print-size
-                    prints generated code size
-    -c, --check-translation
-                    just checks the correctness of Cranelift IR translated from WebAssembly
-    -p, --print     print the resulting Cranelift IR
-    -h, --help      print this help message
-    --set=<set>     configure Cranelift settings
-    --target=<triple>
-                    specify the Cranelift target
-    --version       print the Cranelift version
-
-";
-
-#[derive(Deserialize, Debug)]
-struct Args {
-    cmd_test: bool,
-    cmd_cat: bool,
-    cmd_filecheck: bool,
-    cmd_print_cfg: bool,
-    cmd_compile: bool,
-    cmd_wasm: bool,
-    arg_file: Vec<String>,
-    flag_just_decode: bool,
-    flag_check_translation: bool,
-    flag_print: bool,
-    flag_verbose: bool,
-    flag_set: Vec<String>,
-    flag_target: String,
-    flag_time_passes: bool,
-    flag_print_size: bool,
-    flag_debug: bool,
-}
-
 /// A command either succeeds or fails with an error message.
 pub type CommandResult = Result<(), String>;
 
-/// Parse the command line arguments and run the requested command.
-fn clif_util() -> CommandResult {
-    // Parse command line arguments.
-    let args: Args = Docopt::new(USAGE)
-        .and_then(|d| {
-            d.help(true)
-                .version(Some(format!("Cranelift {}", VERSION)))
-                .deserialize()
-        })
-        .unwrap_or_else(|e| e.exit());
+fn add_input_file_arg<'a>() -> clap::Arg<'a, 'a> {
+    Arg::with_name("file")
+        .required(true)
+        .multiple(true)
+        .value_name("file")
+        .help("Specify file(s) to be used for test")
+}
 
-    if args.flag_debug {
-        pretty_env_logger::init();
-    } else {
-        file_per_thread_logger::initialize(LOG_FILENAME_PREFIX);
+fn add_verbose_flag<'a>() -> clap::Arg<'a, 'a> {
+    Arg::with_name("verbose").short("v").help("Be more verbose")
+}
+
+fn add_time_flag<'a>() -> clap::Arg<'a, 'a> {
+    Arg::with_name("time-passes")
+        .short("T")
+        .help("Print pass timing report for test")
+}
+
+fn add_set_flag<'a>() -> clap::Arg<'a, 'a> {
+    Arg::with_name("set")
+        .long("set")
+        .takes_value(true)
+        .multiple(true)
+        .help("Configure Cranelift settings")
+}
+
+fn add_target_flag<'a>() -> clap::Arg<'a, 'a> {
+    Arg::with_name("target")
+        .takes_value(true)
+        .long("target")
+        .help("Specify the Cranelift target")
+}
+
+fn add_print_flag<'a>() -> clap::Arg<'a, 'a> {
+    Arg::with_name("print")
+        .short("p")
+        .help("Print the resulting Cranelift IR")
+}
+
+/// Takes vector of clap values and converts the values to strings and puts values in the mut vector
+fn get_vec(mut_vec: &mut Vec<String>, argument_vec: Option<clap::Values<'_>>) {
+    if let Some(clap_vec) = argument_vec {
+        for val in clap_vec {
+            mut_vec.push(val.to_string());
+        }
     }
+}
 
-    // Find the sub-command to execute.
-    let result = if args.cmd_test {
-        cranelift_filetests::run(args.flag_verbose, &args.arg_file).map(|_time| ())
-    } else if args.cmd_cat {
-        cat::run(&args.arg_file)
-    } else if args.cmd_filecheck {
-        rsfilecheck::run(&args.arg_file, args.flag_verbose)
-    } else if args.cmd_print_cfg {
-        print_cfg::run(&args.arg_file)
-    } else if args.cmd_compile {
-        compile::run(
-            args.arg_file,
-            args.flag_print,
-            &args.flag_set,
-            &args.flag_target,
-        )
-    } else if args.cmd_wasm {
-        #[cfg(feature = "wasm")]
-        let result = wasm::run(
-            args.arg_file,
-            args.flag_verbose,
-            args.flag_just_decode,
-            args.flag_check_translation,
-            args.flag_print,
-            &args.flag_set,
-            &args.flag_target,
-            args.flag_print_size,
-        );
-
-        #[cfg(not(feature = "wasm"))]
-        let result = Err("Error: clif-util was compiled without wasm support.".to_owned());
-
-        result
-    } else {
-        // Debugging / shouldn't happen with proper command line handling above.
-        Err(format!("Unhandled args: {:?}", args))
+fn add_wasm_or_compile<'a>(cmd: &str) -> clap::App<'a, 'a> {
+    let about_str = match cmd {
+        "wasm" => "Compiles Cranelift IR into target language",
+        "compile" => "Compiles Cranelift IR into target language",
+        // Will not get here, function only called if cmd is wasm or compile
+        _ => "Invalid command",
     };
 
-    if args.flag_time_passes {
-        print!("{}", timing::take_current());
-    }
-
-    result
+    SubCommand::with_name(cmd)
+        .about(about_str)
+        .arg(add_verbose_flag())
+        .arg(add_print_flag())
+        .arg(add_time_flag())
+        .arg(add_set_flag())
+        .arg(add_target_flag())
+        .arg(add_input_file_arg())
 }
 
 fn main() {
-    if let Err(mut msg) = clif_util() {
+    let app_cmds = App::new("Cranelift code generator utility")
+        .version(VERSION)
+        .subcommand(
+            SubCommand::with_name("test")
+                .about("Run Cranelift tests")
+                .arg(add_verbose_flag())
+                .arg(add_time_flag())
+                .arg(add_input_file_arg()),
+        )
+        .subcommand(
+            SubCommand::with_name("cat")
+                .about("Outputs .clif file")
+                .arg(add_input_file_arg()),
+        )
+        .subcommand(
+            SubCommand::with_name("print-cfg")
+                .about("Prints out cfg in dot format")
+                .arg(add_input_file_arg()),
+        )
+        .subcommand(
+            add_wasm_or_compile("compile")
+                .arg(
+                    Arg::with_name("just_decode")
+                        .short("t")
+                        .help("Just decode WebAssembly to Cranelift IR"),
+                )
+                .arg(Arg::with_name("check_translation").short("c").help(
+                    "Just checks the correctness of Cranelift IR translated from WebAssembly",
+                )),
+        )
+        .subcommand(add_wasm_or_compile("wasm"));
+
+    let res_util = match app_cmds.get_matches().subcommand() {
+        ("cat", Some(rest_cmd)) => {
+            let mut file_vec: Vec<String> = Vec::new();
+            get_vec(&mut file_vec, rest_cmd.values_of("file"));
+            cat::run(&file_vec)
+        }
+        ("test", Some(rest_cmd)) => {
+            let mut file_vec: Vec<String> = Vec::new();
+            get_vec(&mut file_vec, rest_cmd.values_of("file"));
+            cranelift_filetests::run(rest_cmd.is_present("time-passes"), &file_vec).map(|_time| ())
+        }
+        ("filecheck", Some(rest_cmd)) => {
+            let mut file_vec: Vec<String> = Vec::new();
+            get_vec(&mut file_vec, rest_cmd.values_of("file"));
+            rsfilecheck::run(&file_vec, rest_cmd.is_present("verbose"))
+        }
+        ("print-cfg", Some(rest_cmd)) => {
+            let mut file_vec: Vec<String> = Vec::new();
+            get_vec(&mut file_vec, rest_cmd.values_of("file"));
+            print_cfg::run(&file_vec)
+        }
+        ("compile", Some(rest_cmd)) => {
+            let mut file_vec: Vec<String> = Vec::new();
+            get_vec(&mut file_vec, rest_cmd.values_of("file"));
+
+            let mut set_vec: Vec<String> = Vec::new();
+            get_vec(&mut set_vec, rest_cmd.values_of("set"));
+
+            let mut target_val: &str = "";
+            if let Some(clap_target_vec) = rest_cmd.values_of("target") {
+                for val in clap_target_vec {
+                    target_val = val;
+                }
+            }
+            compile::run(file_vec, rest_cmd.is_present("print"), &set_vec, target_val)
+        }
+        ("wasm", Some(rest_cmd)) => {
+            let mut file_vec: Vec<String> = Vec::new();
+            get_vec(&mut file_vec, rest_cmd.values_of("file"));
+
+            let mut set_vec: Vec<String> = Vec::new();
+            get_vec(&mut set_vec, rest_cmd.values_of("set"));
+
+            let mut target_val: &str = "";
+            if let Some(clap_target_vec) = rest_cmd.values_of("target") {
+                for val in clap_target_vec {
+                    target_val = val;
+                }
+            }
+
+            #[cfg(feature = "wasm")]
+            let result = wasm::run(
+                file_vec,
+                rest_cmd.is_present("verbose"),
+                rest_cmd.is_present("just-decode"),
+                rest_cmd.is_present("check-translation"),
+                rest_cmd.is_present("print"),
+                &set_vec,
+                target_val,
+                rest_cmd.is_present("print-size"),
+            );
+
+            #[cfg(not(feature = "wasm"))]
+            let result = Err("Error: clif-util was compiled without wasm support.".to_owned());
+
+            result
+        }
+        _ => Err(format!("Invalid subcommand.")),
+    };
+
+    if let Err(mut msg) = res_util {
         if !msg.ends_with('\n') {
             msg.push('\n');
         }
