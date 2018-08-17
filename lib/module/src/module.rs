4263a399f9a7d4c478f39f1e10fd38a9a6b71b24
Dan Gohman <sunfish@mozilla.com>
2018-08-16 17:50:22 -0700

    [SimpleJIT] When finalizing multiple functions, make them all executable at the end.
    Add `publish()` function to cranelift-module's `Backend` trait, which
    allows `finalize_all()` to defer making memory executable until it
    has finished all of the patching it needs to do.
    
    Fixes #468.

--- lib/module/src/module.rs
+++ lib/module/src/module.rs
@@ -584,6 +584,7 @@ where
             )
         };
         self.contents.functions[func].finalized = true;
+        self.backend.publish();
         output
     }
 
@@ -611,6 +612,7 @@ where
             )
         };
         self.contents.data_objects[data].finalized = true;
+        self.backend.publish();
         output
     }
 
@@ -630,6 +632,9 @@ where
                 );
             }
         }
+        for info in self.contents.functions.values_mut() {
+            info.finalized = true;
+        }
         for info in self.contents.data_objects.values() {
             if info.decl.linkage.is_definable() && !info.finalized {
                 self.backend.finalize_data(
@@ -642,12 +647,16 @@ where
                 );
             }
         }
+        for info in self.contents.data_objects.values_mut() {
+            info.finalized = true;
+        }
     }
 
     /// Consume the module and return the resulting `Product`. Some `Backend`
     /// implementations may provide additional functionality available after
     /// a `Module` is complete.
-    pub fn finish(self) -> B::Product {
+    pub fn finish(mut self) -> B::Product {
+        self.backend.publish();
         self.backend.finish()
     }
 }
