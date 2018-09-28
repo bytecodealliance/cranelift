cabf2884455ce898415f565094c664101987348e
Dan Gohman <sunfish@mozilla.com>
2018-09-27 22:01:26 -0700

    Implement wasm RefNull and RefIsNull

--- cranelift-wasm/src/code_translator.rs
+++ cranelift-wasm/src/code_translator.rs
@@ -822,6 +822,12 @@ pub fn translate_operator<FE: FuncEnvironment + ?Sized>(
         Operator::F32Le | Operator::F64Le => {
             translate_fcmp(FloatCC::LessThanOrEqual, builder, state)
         }
+        Operator::RefNull => state.push1(builder.ins().null(environ.reference_type())),
+        Operator::RefIsNull => {
+            let arg = state.pop1();
+            let val = builder.ins().is_null(environ.reference_type(), arg);
+            state.push1(val);
+        }
         Operator::Wake { .. }
         | Operator::I32Wait { .. }
         | Operator::I64Wait { .. }
