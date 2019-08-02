992fb31b099a7c1ba4f1781b4e6420930a9978e9
Andrew Brown <andrew.brown@intel.com>
2019-08-02 11:23:06 -0700

    Add `test run` to cranelift-filetests to allow executing CLIF
    This re-factors the compile/execute parts to a FunctionRunner that is shared between cranelift-filetests and clif-util. CLIF can be now be run using `clif-util run` as well as during `clif-util test` for files with a `test run` header. As before, only functions suffixed with a `run` comment are executed. The `run: fn(...) == ...` expression syntax is left for a subsequent change.

--- src/run.rs
+++ src/run.rs
@@ -1,19 +1,12 @@
 //! CLI tool to compile Cranelift IR files to native code in memory and execute them.
 
 use crate::utils::read_to_string;
-use core::mem;
-use cranelift_codegen::binemit::{Reloc, Stackmap};
-use cranelift_codegen::ir::{ExternalName, Function, JumpTable};
 use cranelift_codegen::isa::TargetIsa;
-use cranelift_codegen::Context;
-use cranelift_codegen::{binemit, ir};
+use cranelift_filetests::FunctionRunner;
 use cranelift_native::builder as host_isa_builder;
 use cranelift_reader::{parse_test, Details, IsaSpec};
-use mmap::{MapOption, MemoryMap};
-use region;
-use region::Protection;
 use std::path::PathBuf;
-use walkdir::{DirEntry, WalkDir};
+use walkdir::WalkDir;
 
 pub fn run(files: Vec<String>, flag_print: bool) -> Result<(), String> {
     let mut total = 0;
@@ -56,7 +49,12 @@ fn iterate_files(files: Vec<String>) -> impl Iterator<Item = PathBuf> {
         .into_iter()
         .flat_map(WalkDir::new)
         .filter(|f| match f {
-            Ok(d) => !is_hidden(d) && !is_directory(d),
+            Ok(d) => {
+                // filter out hidden files (starting with .)
+                !d.file_name().to_str().map_or(false, |s| s.starts_with("."))
+                    // filter out directories
+                    && !d.file_type().is_dir()
+            }
             Err(e) => {
                 println!("Unable to read file: {}", e);
                 false
@@ -66,12 +64,6 @@ fn iterate_files(files: Vec<String>) -> impl Iterator<Item = PathBuf> {
             f.expect("This should not happen: we have already filtered out the errors")
                 .into_path()
         })
-}
-fn is_hidden(d: &DirEntry) -> bool {
-    d.file_name().to_str().map_or(false, |s| s.starts_with("."))
-}
-fn is_directory(d: &DirEntry) -> bool {
-    d.file_type().is_dir()
 }
 
 /// Run all functions in a file that are succeeded by "run:" comments
@@ -83,86 +75,24 @@ fn run_single_file(path: &PathBuf) -> Result<(), String> {
 /// Main body of `run_single_file` separated for testing
 fn run_file_contents(file_contents: String) -> Result<(), String> {
     let test_file = parse_test(&file_contents, None, None).map_err(|e| e.to_string())?;
-    let isa = create_target_isa(test_file.isa_spec)?;
     for (func, Details { comments, .. }) in test_file.functions {
-        if comments.iter().filter(|c| c.text.contains("run")).count() > 0 {
-            run_single_function(func, isa.as_ref())?
+        if comments.iter().any(|c| c.text.contains("run")) {
+            let isa = create_target_isa(&test_file.isa_spec)?;
+            FunctionRunner::new(func, isa).run()?
         }
     }
     Ok(())
 }
 
 /// Build an ISA based on the current machine running this code (the host)
-fn create_target_isa(isa_spec: IsaSpec) -> Result<Box<dyn TargetIsa>, String> {
+fn create_target_isa(isa_spec: &IsaSpec) -> Result<Box<dyn TargetIsa>, String> {
     if let IsaSpec::None(flags) = isa_spec {
         // build an ISA for the current machine
         let builder = host_isa_builder()?;
-        Ok(builder.finish(flags))
+        Ok(builder.finish(flags.clone()))
     } else {
         Err(String::from("A target ISA was specified in the file but should not have been--only the host ISA can be used for running CLIF files"))?
     }
-}
-
-/// Compile and execute a single function, expecting a boolean to be returned; a 'true' value is
-/// interpreted as a successful test execution and mapped to Ok whereas a 'false' value is
-/// interpreted as a failed test and mapped to Err.
-fn run_single_function(func: Function, isa: &dyn TargetIsa) -> Result<(), String> {
-    if !(func.signature.params.is_empty()
-        && func.signature.returns.len() == 1
-        && func.signature.returns.first().unwrap().value_type.is_bool())
-    {
-        return Err(String::from(
-            "Functions must have a signature like: () -> boolean",
-        ));
-    }
-    // TODO observed segfaults with return types of b64
-
-    // set up the context
-    let mut context = Context::new();
-    context.func = func;
-
-    // compile and encode the result to machine code
-    let relocs = &mut NullRelocSink {};
-    let traps = &mut NullTrapSink {};
-    let stackmaps = &mut NullStackmapSink {};
-    let code_info = context.compile(isa).map_err(|e| e.to_string())?;
-    let code_page = MemoryMap::new(code_info.total_size as usize, &[MapOption::MapWritable])
-        .map_err(|e| e.to_string())?;
-    let callable_fn: fn() -> bool = unsafe {
-        context.emit_to_memory(isa, code_page.data(), relocs, traps, stackmaps);
-        region::protect(code_page.data(), code_page.len(), Protection::ReadExecute)
-            .map_err(|e| e.to_string())?;
-        mem::transmute(code_page.data())
-    };
-
-    // execute
-    match callable_fn() {
-        true => Ok(()),
-        false => Err(format!("Failed: {}", context.func.name.to_string())),
-    }
-}
-
-/// Relocation helper
-struct NullRelocSink {}
-
-impl binemit::RelocSink for NullRelocSink {
-    fn reloc_ebb(&mut self, _: u32, _: Reloc, _: u32) {}
-    fn reloc_external(&mut self, _: u32, _: Reloc, _: &ExternalName, _: i64) {}
-    fn reloc_jt(&mut self, _: u32, _: Reloc, _: JumpTable) {}
-}
-
-/// Trap helper
-struct NullTrapSink {}
-
-impl binemit::TrapSink for NullTrapSink {
-    fn trap(&mut self, _: binemit::CodeOffset, _: ir::SourceLoc, _: ir::TrapCode) {}
-}
-
-/// Stack helper
-struct NullStackmapSink {}
-
-impl binemit::StackmapSink for NullStackmapSink {
-    fn add_stackmap(&mut self, _: u32, _: Stackmap) {}
 }
 
 #[cfg(test)]
