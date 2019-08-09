343db39e8269c74735cd76e60a9fd4f556d3a2a3
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2019-08-09 18:29:33 +0200

    Disable branch-splitting when basic block feature is not set.

--- cranelift-codegen/src/regalloc/branch_splitting.rs
+++ cranelift-codegen/src/regalloc/branch_splitting.rs
@@ -3,6 +3,7 @@
 //! The `minimal` and `tree` register allocators require this. One of the reason for splitting edges
 //! is to be able to insert `copy` and `regmove` instructions between a conditional branch and the
 //! following terminator.
+#![cfg(feature = "basic-blocks")]
 
 use std::vec::Vec;
 
