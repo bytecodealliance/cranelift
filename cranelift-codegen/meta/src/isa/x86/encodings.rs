36823370eafe4b28758a9f01f2bd10d46df4a23a
Andrew Brown <andrew.brown@intel.com>
2019-08-22 09:34:33 -0700

    Revert "Use 32-bit encoding for bconst.b64 to avoid new recipe"
    This reverts commit 56f07338629b8580392266e7aea0220c902a9a79.

--- cranelift-codegen/meta/src/isa/x86/encodings.rs
+++ cranelift-codegen/meta/src/isa/x86/encodings.rs
@@ -520,6 +520,7 @@ pub fn define(
     let rec_pu_id_bool = r.template("pu_id_bool");
     let rec_pu_id_ref = r.template("pu_id_ref");
     let rec_pu_iq = r.template("pu_iq");
+    let rec_pu_iq_bool = r.template("pu_iq_bool");
     let rec_pushq = r.template("pushq");
     let rec_ret = r.template("ret");
     let rec_r_ib = r.template("r_ib");
@@ -683,7 +684,10 @@ pub fn define(
     for &ty in &[B1, B8, B16, B32] {
         e.enc_both(bconst.bind(ty), rec_pu_id_bool.opcodes(vec![0xb8]));
     }
-    e.enc64(bconst.bind(B64), rec_pu_id_bool.opcodes(vec![0xb8]));
+    e.enc64(
+        bconst.bind(B64),
+        rec_pu_iq_bool.opcodes(vec![0xb8]).rex().w(),
+    );
 
     // Shifts and rotates.
     // Note that the dynamic shift amount is only masked by 5 or 6 bits; the 8-bit
