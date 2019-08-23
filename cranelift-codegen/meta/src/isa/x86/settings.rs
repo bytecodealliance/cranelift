d6a64734d59238fd7b119fdd492b5797a96e3b2d
Ujjwal Sharma <usharma1998@gmail.com>
2019-08-23 14:59:48 +0530

    Revert "Add x86-specific SIMD settings, e.g. SSE2"
    This reverts commit 66fb5e2d3b90074924ea537bd82df49c4f51aad1.

--- cranelift-codegen/meta/src/isa/x86/settings.rs
+++ cranelift-codegen/meta/src/isa/x86/settings.rs
@@ -30,14 +30,8 @@ pub fn define(shared: &SettingGroup) -> SettingGroup {
         false,
     );
 
-    let shared_enable_simd = shared.get_bool("enable_simd");
-
-    settings.add_predicate("use_ssse3", predicate!(shared_enable_simd && has_ssse3));
-    settings.add_predicate("use_sse41", predicate!(shared_enable_simd && has_sse41));
-    settings.add_predicate(
-        "use_sse42",
-        predicate!(shared_enable_simd && has_sse41 && has_sse42),
-    );
+    settings.add_predicate("use_sse41", predicate!(has_sse41));
+    settings.add_predicate("use_sse42", predicate!(has_sse41 && has_sse42));
     settings.add_predicate("use_popcnt", predicate!(has_popcnt && has_sse42));
     settings.add_predicate("use_bmi1", predicate!(has_bmi1));
     settings.add_predicate("use_lzcnt", predicate!(has_lzcnt));
