943c73af16a7a6f4e221a5b29783141ac459c389
Andrew Brown <andrew.brown@intel.com>
2019-12-11 10:32:12 -0800

    Add Signature::param_types method; only collect normal parameters in both this and Signature::return_types

--- cranelift-codegen/src/ir/extfunc.rs
+++ cranelift-codegen/src/ir/extfunc.rs
@@ -103,9 +103,22 @@ impl Signature {
             > 1
     }
 
-    /// Collect the return types of the signature.
+    /// Collect the normal parameter types of the signature; see `[ArgumentPurpose::Normal]`.
+    pub fn param_types(&self) -> Vec<Type> {
+        self.params
+            .iter()
+            .filter(|ap| ap.purpose == ArgumentPurpose::Normal)
+            .map(|ap| ap.value_type)
+            .collect()
+    }
+
+    /// Collect the normal return types of the signature; see `[ArgumentPurpose::Normal]`.
     pub fn return_types(&self) -> Vec<Type> {
-        self.returns.iter().map(|ap| ap.value_type).collect()
+        self.returns
+            .iter()
+            .filter(|ap| ap.purpose == ArgumentPurpose::Normal)
+            .map(|ap| ap.value_type)
+            .collect()
     }
 }
 
